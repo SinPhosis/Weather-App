10c7d8bac01379f94b6ac95304a3ae675c75dbf9
SinPhosis <sionclasher@gmail.com>
2025-01-17 17:56:00 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ function App() {
   const [searchValue, setSearchValue] = useState("");
   const [allCities, setAllCities] = useState([]);
   const [selectedCity, setSelectedCity] = useState("Ulaanbaatar, Mongolia");
-  const [weatherData, setWeatherData] = useState({});
+  const [weatherData, setWeatherData] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [filteredData, setFilteredData] = useState([]);
+  const [currentDate, setCurrentDate] = useState("");
+  const [weatherCondition, setWeatherCondition] = useState(null);
+  const [nightTemperature, setNightTemperature] = useState(null);
+  const [nightCondition, setNightCondition] = useState(null);
 
   const weatherApiKey = "56294f0115d9424ca0e84959251501";
 
@@ -33,7 +37,23 @@ function App() {
         `https://api.weatherapi.com/v1/forecast.json?key=${weatherApiKey}&q=${selectedCity}`
       );
       const result = await response.json();
-      setWeatherData(result);
+
+      /* day temperature */
+      const temp = result.forecast.forecastday[0].day.maxtemp_c;
+
+      setWeatherData(temp);
+
+      /* day condition */
+      const condition = result.forecast.forecastday[0].day.condition.text;
+      setWeatherCondition(condition);
+
+      /* night temperature */
+      const nightT = result.forecast.forecastday[0].day.mintemp_c;
+      setNightTemperature(nightT);
+
+      /* night condition */
+      const nightC = result.forecast.forecastday[0].day.condition.text;
+      setNightCondition(nightC);
     } catch (error) {
       console.log(error);
     } finally {
@@ -63,41 +83,53 @@ function App() {
   useEffect(() => {
     if (selectedCity) {
       getWeatherData();
-    } 
+    }
   }, [selectedCity]);
+
+  useEffect(() => {
+    const date = new Date();
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    setCurrentDate(date.toLocaleDateString(undefined, options));
+  }, []);
 
   if (isLoading) {
     return (
       <>
-      <div class="animate-spin inline-block size-6 border-[3px] ml-[550px] mt-[550px] border-current border-t-transparent text-black rounded-full" role="status" aria-label="loading">
-  <span class="sr-only">Loading...</span>
-</div>
-<div class="animate-spin inline-block size-6 border-[3px] ml-[800px] mt-[550px] border-current border-t-transparent text-black rounded-full" role="status" aria-label="loading">
-  <span class="sr-only">Loading...</span>
-</div>
-</>
+        <div
+          className="animate-spin inline-block size-6 border-[3px] ml-[550px] mt-[550px] border-current border-t-transparent text-black rounded-full"
+          role="status"
+          aria-label="loading"
+        >
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div
+          className="animate-spin inline-block size-6 border-[3px] ml-[800px] mt-[550px] border-current border-t-transparent text-black rounded-full"
+          role="status"
+          aria-label="loading"
+        >
+          <span className="sr-only">Loading...</span>
+        </div>
+      </>
     );
   }
-
-  const temperature = weatherData?.current?.temp_c || "N/A";
-  const condition = weatherData?.current?.condition?.text || "N/A";
 
   return (
     <div className="w-[1600px] h-[1200px] justify-center items-center flex m-40">
       <div className="w-[800px] h-[1200px] relative bg-gray-100 rounded-l-xl flex-col justify-start items-start inline-flex overflow-hidden">
         <div className="w-[1740px] h-[1200px] relative">
-          <div className="w-[340px] h-[340px] absolute left-[630px] top-[430px] bg-gray-100 absolute  rounded-full border border-gray-900">
-          <img
-            className="absolute block right-[200px] top-[120px] "
-            src="./Group 4.png"
-          ></img>
+          <div className="w-[140px] h-[140px] top-[530px] left-[730px] bg-gray-100 absolute rounded-full border border-gray-200">
+            <img
+              className="absolute block right-[75px] top-[25px]"
+              src="./Group 4.png"
+            ></img>
           </div>
-          <div className="w-[540px] h-[540px] absolute top-[330px] left-[530px] opacity-10 rounded-full border border-gray-900" />
-          <div className="w-[940px] h-[940px] absolute bottom-[130px] left-[330px] opacity-10 rounded-full border border-gray-900" />
-          <div className="w-[1340px] h-[1200px] absolute left-[130px] bottom-[-70px] opacity-5 rounded-full border border-gray-900" />
-          <div className="w-[1740px] h-[1200px] absolute bottom-[-270px] left-[-70px]  opacity-5 rounded-full border border-gray-900" />
+          <div className="w-[340px] h-[340px] absolute left-[630px] top-[430px] rounded-full border border-gray-200" />
+          <div className="w-[540px] h-[540px] absolute left-[530px] top-[330px] rounded-full border border-gray-200" />
+          <div className="w-[940px] h-[940px] absolute left-[330px] top-[130px] rounded-full border border-gray-200" />
+          <div className="w-[1340px] h-[1340px] absolute left-[130px] top-[-70px] rounded-full border border-gray-200" />
+          <div className="w-[1740px] h-[1740px] absolute left-[-70px] bottom-[-270px] rounded-full border border-gray-200" />
           <img
-            className="absolute left-[130px] top-[170px] "
+            className="absolute left-[130px] top-[170px]"
             src="./sun-little.webp"
           ></img>
           <div className="w-[800px] h-[1200px] relative">
@@ -133,42 +165,44 @@ function App() {
               />
               <div className="h-[91px] flex-col justify-start items-start inline-flex">
                 <div className="text-gray-500 text-lg top-[56px] left-[40px] absolute font-medium">
-                  January 15, 2025
+                  {currentDate}
                 </div>
                 <div className="text-gray-900 text-5xl top-[81px] left-[40px] absolute font-extrabold">
                   {selectedCity}
                 </div>
               </div>
               <div className="h-[230px] flex-col justify-start items-start inline-flex">
-                <div className="text-gray-900 text-[144px] left-[48px] bottom-[153px] absolute bg-gradient-to-b from-gray-900 to-gray-300 bg-clip-text text-transparent font-black">
-                  {temperature}˚
+                <div className="text-gray-900 text-[144px] left-[25px] bottom-[153px] absolute bg-gradient-to-b from-gray-900 to-gray-300 bg-clip-text text-transparent font-black">
+                  {weatherData}˚
                 </div>
                 <div className="text-[#fe8e26] absolute bottom-[120px] left-[48px] text-2xl font-extrabold">
-                  {condition}
+                  {weatherCondition}
                 </div>
               </div>
             </div>
 
             <div className="w-[318px] h-8 bottom-[182px] left-[241px] absolute justify-between items-end inline-flex">
-              <img src="./Home.png" alt="Home Icon" />
-              <img src="./Pin.png" alt="Pin Icon" />
-              <img src="./Heart.png" alt="Heart Icon" />
-              <img src="./User.png" alt="User Icon" />
+              <img src="./Home.png" />
+              <img src="./Pin.png" />
+              <img src="./Heart.png" />
+              <img src="./User.png" />
             </div>
           </div>
         </div>
       </div>
       <div className="w-[800px] h-[1200px] bg-[#0f141e] relative flex-col justify-start items-start inline-flex overflow-hidden">
-        <div className="w-[340px] h-[340px] top-[430px] right-[630px] bg-gray-100 absolute  rounded-full border border-white">
+        <div className="w-[140px] h-[140px] top-[530px] right-[730px] bg-gray-100 absolute rounded-full border border-gray-200">
           <img
-            className="absolute right-[80px] top-[120px] " 
+            className="absolute right-[15px] top-[25px]"
             src="./Vector.png"
           ></img>
         </div>
+        <div className="w-[340px] h-[340px] top-[430px] right-[630px] opacity-10 absolute rounded-full border border-white" />
         <div className="w-[540px] h-[540px] absolute top-[330px] right-[530px] opacity-10 rounded-full border border-white" />
-        <div className="w-[940px] h-[940px] absolute bottom-[130px] right-[330px] opacity-10 rounded-full border border-white" />
-        <div className="w-[1340px] h-[1200px] absolute right-[130px] bottom-[-70px] opacity-5 rounded-full border border-white" />
-        <div className="w-[1740px] h-[1200px] absolute bottom-[-270px] right-[-70px]  opacity-5 rounded-full border border-white" />
+        <div className="w-[940px] h-[940px] absolute top-[130px] right-[330px] opacity-10 rounded-full border border-white" />
+        <div className="w-[1340px] h-[1340px] absolute top-[-70px] right-[130px] opacity-5 rounded-full border border-white" />
+        <div className="w-[1740px] h-[1740px] absolute top-[-270px] right-[-70px] opacity-5 rounded-full border border-white" />
+
         <img
           className="absolute bottom-[104px] right-[145px]"
           src="./Ellipse 22.png"
@@ -177,7 +211,7 @@ function App() {
           <div className="w-[398px] h-[504px] absolute bottom-[320px] right-[8px] bg-gradient-to-b from-gray-800 to-gray-900 rounded-[42px]">
             <div className="h-[91px] flex-col justify-start items-start inline-flex">
               <div className="text-gray-400 text-lg font-medium font-['Manrope Fallback'] absolute left-[40px] top-[56px]">
-                January 15, 2025
+                {currentDate}
               </div>
               <div className="text-white text-5xl font-extrabold font-['Manrope Fallback'] absolute top-[81px] left-[40px]">
                 {selectedCity}
@@ -189,17 +223,17 @@ function App() {
               <div className="w-[277px] h-[277px] top-[171px] left-[69px] relative">
                 <img src="./shadow.png"></img>
                 <img
-                  className="absolute bottom-[12px] right-[11px] w-[264.89px] h-[264.89px] "
+                  className="absolute bottom-[12px] right-[11px] w-[264.89px] h-[264.89px]"
                   src="./icon.png"
                 ></img>
               </div>
             </div>
           </div>
           <div className="w-[396px] h-[197px] bg-gradient-to-b from-gray-300 to-gray-700 bg-clip-text text-transparent text-[144px] absolute bottom-[167px] font-extrabold font-['Manrope Fallback']">
-            {temperature}˚
+            {nightTemperature}˚
           </div>
           <div className="text-[#777cce] text-2xl font-extrabold font-['Manrope Fallback'] absolute bottom-[134px] left-[48px]">
-            {condition}
+            {nightCondition}
           </div>
           <div className="w-[318px] h-8 absolute bottom-[48px] left-[48px] justify-between items-end inline-flex">
             <img src="./Home.png" />
